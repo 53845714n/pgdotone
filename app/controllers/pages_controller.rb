09f68535ad077d77bae8d6535ecd2c79477a14f2
LagartoSeb <ssebastian.a027@gmail.com>
2016-04-06 00:45:32 -0300

    finish static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
 	def index
 	end
+
+	def proyectos
+	end
+
+	def modelos_ficha
+	end
+
+	def proyectos_ficha
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   #resources :pages
+  get '/proyectos-ficha' => 'pages#proyectos_ficha', as: 'proyectos_ficha'
+  get '/modelos-ficha'   => 'pages#modelos_ficha',   as: 'modelos_ficha'
+  get '/proyectos'       => 'pages#proyectos'
   root 'pages#index'
 end
