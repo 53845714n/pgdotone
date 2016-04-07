05426fb01ff8870d118518287e11e5ef9cd1227d
LagartoSeb <ssebastian.a027@gmail.com>
2016-04-06 21:22:17 -0300

    contact and its styles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,4 +10,7 @@ class PagesController < ApplicationController
 
 	def proyectos_ficha
 	end
+
+	def contacto
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get '/proyectos-ficha' => 'pages#proyectos_ficha', as: 'proyectos_ficha'
   get '/modelos-ficha'   => 'pages#modelos_ficha',   as: 'modelos_ficha'
   get '/proyectos'       => 'pages#proyectos'
+  get '/contacto'        => 'pages#contacto'
   root 'pages#index'
 end
