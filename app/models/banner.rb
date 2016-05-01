ed75cd9b5c7656dd391189bf17fccabe73e19f56
Sebastián Palma <ssebastian.a027@gmail.com>
2016-04-30 23:10:58 -0300

    holy video

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -1,7 +1,7 @@
 class Banner < ActiveRecord::Base
 	mount_uploader :image, ImageBannerUploader
 	mount_uploader :background_image, BackgroundImageUploader
-	mount_uploader :video, VideoUploaders
+	mount_uploader :video, VideoUploader
 
     def set_success(format, opts)
     	self.success = true
